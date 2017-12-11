4bf52f8e38105804006171e4acdf859cfc05d505
DevLock <Ferchulin6489@Github.com>
2017-12-11 09:06:54 +0100

    Files

---
+++ ClienteC1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClienteC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando...");
+            Conn Sock = new Conn();
+            Sock.Config_Server();
+            Console.WriteLine("Press Enter from initialize server.");
+            Console.ReadLine();
+            Console.WriteLine("Init...");
+            Sock.Connect();
+            Console.WriteLine("Press Enter from Disconnect.");
+            Console.ReadLine();
+            Sock.Disconnect();
+            Console.WriteLine("Close read.");
+            Console.WriteLine("Press Enter from exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
